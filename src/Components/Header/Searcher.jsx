d7aa6317647c5c6ce33b03be8e2f366bca815fa3
juliafaraudello <julifaraudello@gmail.com>
2022-11-04 21:39:07 +0100

    update range pikcer

--- src/Components/Header/Searcher.jsx
+++ src/Components/Header/Searcher.jsx
@@ -1,15 +1,42 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState } from 'react'
 import search from "../../Icons/search.png"
 import ciudades from "../../JSON/ciudades.json"
-import Calendar from 'react-calendar';
+import { DateRange} from 'react-date-range';
+// import 'react-datepicker/dist/react-datepicker.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import format from 'date-fns/format'
+import { addDays } from 'date-fns'
 
 const Searcher = () => {
-const [date,setDate] = useState(Date.now())
-const today = (new Date().getFullYear())+'-'+((new Date()).getMonth()+1)+'-'+((new Date()).getDay());
+  const [range, setRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(),7),
+      key: 'selection'
+    }
+  ])
 
-const onChange = date => {
-  setDate(date)
-}
+  const [open, setOpen] = useState(false)
+  const refOne = useRef(null)
+
+  useEffect(() => {
+    document.addEventListener("keydown",hideOnEscape,true)
+    document.addEventListener("click",hideOnClickOutside,true)
+  }, [])
+
+  const hideOnEscape = (e) =>{
+    console.log(e.key)
+    if(e.key === "Escape"){
+      setOpen(false)
+    }
+  }
+
+  const hideOnClickOutside = (e) =>{
+    if (refOne.current && !refOne.current.contains(e.target)){
+      setOpen(false)
+    }
+  }
   //console.log(today)
   return (
     <div className='form-container'>
@@ -23,9 +50,22 @@ const onChange = date => {
         </select>
         {/*<input className='search-input' type="text" placeholder='Dónde quieres ir?' id="search-location"/>*/}
         <a className='vertical-line'></a>
-        <input className='search-input' type="date" id="search-date" min={today}/>
-        <a className='vertical-line'></a>
-        <input className='search-input' type="date" id="search-date"/>
+        <input value={ ` ${format(range[0].startDate, "dd/MM/yyyy")} to ${format(range[0].endDate, "dd/MM/yyyy")} ` } readOnly className='inputBox' onClick={() => setOpen(open => !open)}/> 
+        <div className='calendarWrap'> 
+          <div ref={refOne}>
+            {open &&
+              <DateRange
+                oneChange = {item => setRange([item.selection])} 
+                editableDateInputs={true}
+                moveRangeOnFirstSelection={false}
+                ranges={range}
+                months={2}
+                direction='horizontal'
+                className='calendarElement'
+              />
+             }
+          </div>
+        </div> 
         <a className='vertical-line'></a>
         <input className='search-input' type="text" placeholder='Cuántas personas?' id="search-people"/>
         <button type="submit"><img src={search} className="search-img search-input"/></button>
