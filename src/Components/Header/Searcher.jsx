d84a991263c713d18a1d51f27501f88b14913704
juliafaraudello <julifaraudello@gmail.com>
2022-11-18 23:02:41 +0100

    update nav searcher

--- src/Components/Header/Searcher.jsx
+++ src/Components/Header/Searcher.jsx
@@ -43,13 +43,16 @@ const Searcher = () => {
     {/*<div className="calendar-searcher">
         <Calendar className="calendar-searcher" showDoubleView={true} onChange={onChange} value={date}></Calendar>
     </div>*/}
-    <form className='searcher-nav'>
+    <div className='city-search'>
+    {/* <form className='searcher-nav'> */}
         <select required name="city" id="city search-location" className='search-input'>
         <option className="placeholder-selected" value="" disabled selected hidden> A dónde vamos?</option>
           {ciudades.map(e=> {return(<option value={e}>{e}</option>)})}
         </select>
         {/*<input className='search-input' type="text" placeholder='Dónde quieres ir?' id="search-location"/>*/}
-        <a className='vertical-line'></a>
+    </div>
+        {/* <a className='vertical-line'></a> */}
+    <div className='container-calendar'>
         <input value={ ` ${format(range[0].startDate, "dd/MM/yyyy")} to ${format(range[0].endDate, "dd/MM/yyyy")} ` } readOnly className='inputBox' onClick={() => setOpen(open => !open)}/> 
         <div className='git'> 
           <div ref={refOne}>
@@ -66,12 +69,11 @@ const Searcher = () => {
              }
           </div>
         </div> 
-        <a className='vertical-line'></a>
-        <input className='search-input' type="text" placeholder='Cuántas personas?' id="search-people"/>
-        <div className='button-search'>
-          <button type="submit">Buscar</button>
-        </div>
-    </form>
+      </div>
+    {/* </form> */}
+    <div className='searcher-button'>
+          <button clasName='button-style' type="submit">Buscar</button>
+    </div>
     </div>
   )
 }
