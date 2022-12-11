a2e6202b84185bbc6a5f472c20d9b046367fcde7
juliafaraudello <julifaraudello@gmail.com>
2022-12-11 17:23:00 +0100

    search city

--- src/Components/Header/Searcher.jsx
+++ src/Components/Header/Searcher.jsx
@@ -5,6 +5,7 @@ import { DateRange} from 'react-date-range';
 // import 'react-datepicker/dist/react-datepicker.css';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import AsyncSelect, { StylesConfig } from 'react-select/async'
 import format from 'date-fns/format'
 import { addDays } from 'date-fns'
 import "../../Style/home.css"
@@ -38,18 +39,59 @@ const Searcher = () => {
       setOpen(false)
     }
   }
+
+  // Estilos y datos del Select
+
+
+
+  const Ciudades = [
+    { label: 'Cordoba', value: '1' },
+    { label: 'Buenos Aires', value: '2' },
+    { label: 'Rosario', value: '3' },
+    { label: 'Mendonza', value: '4' },
+  ];
+
+  const filterCiudades = (inputValue: string) => {
+    return Ciudades.filter((i) =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  };
+  
+  const loadOptions = (
+    inputValue: string,
+    callback: (options: Ciudades[]) => void
+  ) => {
+    setTimeout(() => {
+      callback(filterCiudades(inputValue));
+    }, 1000);
+  };
+
+
   //console.log(today)
   return (
     <div className='form-container'>
     {/*<div className="calendar-searcher">
         <Calendar className="calendar-searcher" showDoubleView={true} onChange={onChange} value={date}></Calendar>
     </div>*/}
-    <div className='city-search'>
-        <select required name="city" id="city search-location" className='search-input'>
-        <option className="placeholder-selected" value="" disabled selected hidden> A dónde vamos?</option>
-          {ciudades.map(e=> {return(<option value={e}>{e}</option>)})}
-        </select>
-    </div>
+    {/* <div className='city-search'> */}
+        <AsyncSelect 
+            className="select-custom-class"
+            showArrow={false}
+            placeholder="Elija un destino"
+            cacheOptions loadOptions={loadOptions} defaultOptions
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: 8,
+              height: '50px',
+              width: '422px',
+              colors: {
+                ...theme.colors,
+                primary25: 'aquamarine',
+                primary: 'black',
+              },
+            })}  
+        />
+    {/* </div> */}
         {/* <a className='vertical-line'></a> */}
     <div className='container-calendar'>
         <input value={ ` ${format(range[0].startDate, "dd/MM/yyyy")} to ${format(range[0].endDate, "dd/MM/yyyy")} ` } readOnly className='inputBox' onClick={() => setOpen(open => !open)}/> 
